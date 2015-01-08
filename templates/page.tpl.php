5c2000eac6c37a916115c8f2fcae7ff83319ca3f
gbelot2003 <gbelot2003@hotmail.com>
2015-01-08 09:06:17 +0000

    Placing title before breadcrumbs

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -151,14 +151,14 @@
       <?php endif; ?>
 
       <a id="main-content"></a>
-
-      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
-
+      
       <?php if ($title && !$is_front): ?>
         <?php print render($title_prefix); ?>
         <h1 id="page-title" class="title"><?php print $title; ?></h1>
         <?php print render($title_suffix); ?>
       <?php endif; ?>
+
+      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
 
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
